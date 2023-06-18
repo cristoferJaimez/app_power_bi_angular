6e29cfb058482dc373a3276c0c6fbccdaf360e62
cristoferjaimez <jaimez07788@gmail.com>
2023-06-17 21:07:59 -0500

    back route power bi 40%

--- back_power/routes/routes.js
+++ back_power/routes/routes.js
@@ -8,7 +8,7 @@ const connection = require('../data/conex'); // Importa la conexión a la base d
 const axios = require('axios');
 const https = require('https');
 const { ClientSecretCredential } = require("@azure/identity");
-
+const qs = require('qs');
 
 // Ruta del login para iniciar session
 router.post('/login', (req, res) => {
@@ -65,10 +65,10 @@ router.get('/verify-token', authenticateToken, (req, res) => {
 });
 
 
+router.get('/obtener-informes/:id', async (req, res) => {
 
-// Ruta para obtener un informe de Power BI
-router.get('/powerbi-report-details/:id', async (req, res) => {
   try {
+
     const reportId = req.params.id;
     //console.log(reportId);
     const query = `CALL select_post(?)`;
@@ -80,92 +80,50 @@ router.get('/powerbi-report-details/:id', async (req, res) => {
         return res.status(404).json({ message: 'Informe no encontrado' });
       }
 
-      const id_report = results[0][0].id_report; // Obtiene el id del informe desde los resultados del procedimiento almacenado
-      const embedUrl = results[0][0].url_report; // Obtiene la URL del informe desde los resultados del procedimiento almacenado
-
-      const credential = new ClientSecretCredential(
-        process.env.AZURE_TENANT_ID,
-        process.env.AZURE_CLIENT_ID,
-        process.env.AZURE_CLIENT_SECRET
-      );
-
-      try {
-        const tokenResponse = await credential.getToken("https://analysis.windows.net/powerbi/api/.default");
-        //console.log('Token obtenido:', tokenResponse.token); // Imprime el token en el registro
-
-        console.log(tokenResponse.token);
-
-        // Obtener información del usuario autenticado
-        const userApiResponse = await axios.get('https://graph.microsoft.com/v1.0/me', { headers: { Authorization: `Bearer ${tokenResponse.token}` } });
-        const user = userApiResponse.data;
-
-        // Mostrar los datos del usuario en la consola
-        console.log('Datos del usuario:');
-        console.log('ID:', user.id);
-        console.log('Nombre:', user.displayName);
-        console.log('Email:', user.mail);
-
-
-        const reportDetails = {
-          reportId: id_report,
-          embedUrl: embedUrl,
-          accessToken: tokenResponse.token,
-        };
-
-
-        // Utiliza el accessToken para acceder a tus informes de Power BI
-
-        res.json(reportDetails);
-      } catch (error) {
-        console.error('Error al obtener el token:', error.message);
-        res.status(500).send('Error al obtener el token');
-      }
+      id_report = results[0][0].id_report; // Obtiene el id del informe desde los resultados del procedimiento almacenado  
     });
-  } catch (error) {
-    console.error('Error en la ruta /powerbi-report-details:', error.message);
-    res.status(500).send('Error en la ruta /powerbi-report-details');
-  }
-});
 
-
-router.post('/get-token-and-url', async (req, res) => {
-  try {
-    const scope = 'https://analysis.windows.net/powerbi/api/.default';
     const tokenEndpoint = `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/token`;
-    
-    const requestBody = new URLSearchParams();
-    requestBody.append('client_id', process.env.AZURE_CLIENT_ID);
-    requestBody.append('client_secret', process.env.AZURE_CLIENT_SECRET);
-    requestBody.append('grant_type', 'client_credentials');
-    requestBody.append('scope', scope);
+    const resource = 'https://analysis.windows.net/powerbi/api';
 
-    const response = await axios.post(tokenEndpoint, requestBody);
-    const token = response.data.access_token;
-   
-    const headers = {
-      Authorization: `Bearer ${token}`
+    const params = {
+      grant_type: 'client_credentials',
+      client_id: process.env.AZURE_CLIENT_ID,
+      client_secret: process.env.AZURE_CLIENT_SECRET,
+      scope: `${resource}/.default`,
     };
 
-    const reportId = "f71dd63e-24eb-4f63-b817-6fd80f8a950";
-    const apiUrl = `https://api.powerbi.com/v1.0/myorg/reports/${reportId}`;
+    const response = await axios.post(tokenEndpoint, qs.stringify(params), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
 
-    try {
-      const reportResponse = await axios.get(apiUrl, { headers });
-      const embedUrl = reportResponse.data.embedUrl;
-      console.log(reportResponse);
-      res.json({ token, embedUrl });
-    } catch (error) {
-      console.error('Error al obtener la URL del informe:', error.message);
-      res.status(500).json({ error: 'Error al obtener la URL del informe', details: error });
-    }
+    const accessToken = response.data.access_token;
+
+    const apiUrl = 'https://api.powerbi.com/v1.0/myorg/reports';
+    const reportsResponse = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+
+    //lista de reportes
+    const reports = reportsResponse.data.value;
+    
+    res.json({ reports, reportUrl });
   } catch (error) {
-    console.error('Error al obtener el token:', error.message);
-    res.status(500).json({ error: 'Error al obtener el token', details: error.response.data });
+    console.error('Error:', error.message);
+
+    if (error.response) {
+      console.error('Response Status:', error.response.status);
+      console.error('Response Data:', error.response);
+    }
+
+    res.status(500).json({ error: 'Error al obtener los informes de Power BI' });
   }
 });
-
-
-
 
 
 module.exports = router;
